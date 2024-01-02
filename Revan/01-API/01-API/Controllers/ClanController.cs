4a9834d75ed8a33f507d8591b3725bb76067a83f
Revannn <rghofur053@gmail.com>
2024-01-02 16:16:06 +0700

    done

---
+++ Revan/01-API/01-API/Controllers/ClanController.cs
@@ -0,0 +1,21 @@
+﻿using _01_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // mengambil singleton yang dibuat kedalam parameter constructor
+    public class ClanController(EsemkaHeroContext context) : ControllerBase
+    {
+        // assign context pada parameter scope ke dalam property context
+        private readonly EsemkaHeroContext context = context;
+
+        [HttpGet("/ambil-data")]
+        public List<Clan> All()
+        {
+            return this.context.Clans.ToList();
+        }
+    }
+}
