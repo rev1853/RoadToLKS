3e64d874d2ae986e98021c60dbfd2e9159fe4c9c
daffaputra09 <daffaputra863@gmail.com>
2024-01-02 16:29:21 +0700

    add FirstAPI project

---
+++ DAFFA_SMKN_4_MALANG/FirstAPI/FirstAPI/Controllers/ClanController.cs
@@ -0,0 +1,19 @@
+﻿using FirstAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClanController(EsemkaHeroContext context) : ControllerBase
+    {
+        private readonly EsemkaHeroContext context = context;
+
+        [HttpGet("/clan")]
+        public List<Clan> All()
+        {
+            return this.context.Clans.ToList();
+        }
+    }
+}
