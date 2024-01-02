19ae14be17594a9a6ddf57878467ce3a29a1d9bc
rodhifirmansyah <rodhifirmansyah6527@gmail.com>
2024-01-02 18:38:40 +0700

    testing

---
+++ RODHI_SMKN_1_PASURUAN/Belajar_WebApi_01/Belajar_WebApi_01/Belajar_WebApi_01/Controllers/ClanController.cs
@@ -0,0 +1,21 @@
+﻿using Belajar_WebApi_01.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Belajar_WebApi_01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class ClanController(EsemkaHeroContext context) : ControllerBase
+    {
+        EsemkaHeroContext context = context;
+
+        [HttpGet("clan")]
+        public List<Clan> c()
+        {
+            return context.Clans.ToList();
+        }
+    }
+}
