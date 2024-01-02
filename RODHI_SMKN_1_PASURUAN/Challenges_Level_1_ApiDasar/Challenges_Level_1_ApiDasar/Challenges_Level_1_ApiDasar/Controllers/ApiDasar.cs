80c701d7fff9db25b33e0b418657a96a0ad12797
rodhifirmansyah <rodhifirmansyah6527@gmail.com>
2024-01-02 21:45:33 +0700

    Challenges_Level_1_ApiDasar

---
+++ RODHI_SMKN_1_PASURUAN/Challenges_Level_1_ApiDasar/Challenges_Level_1_ApiDasar/Challenges_Level_1_ApiDasar/Controllers/ApiDasar.cs
@@ -0,0 +1,38 @@
+﻿using Challenges_Level_1_ApiDasar.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Challenges_Level_1_ApiDasar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiDasar(EsemkaHeroContext context) : ControllerBase
+    {
+        EsemkaHeroContext context = context;
+
+
+        [HttpGet("GET")]
+        public string g()
+        {
+            return "Hello there, this is the GET ";
+        }
+        
+        [HttpPost("POST")]
+        public string p()
+        {
+            return "Hello there, this is the POST ";
+        } 
+        
+        [HttpPut("PUT")]
+        public string a()
+        {
+            return "Hello there, this is the PUT ";
+        }
+        
+        [HttpDelete("DELETE")]
+        public string d()
+        {
+            return "Hello there, this is the DELETE ";
+        } 
+    }
+}
