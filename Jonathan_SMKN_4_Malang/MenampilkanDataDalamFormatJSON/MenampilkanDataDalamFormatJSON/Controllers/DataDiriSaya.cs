1e6616577af648b9e10f89419fbbdf373529f40d
ZhayaGT <jonathanemmanuel0107@gmail.com>
2024-01-03 11:16:15 +0700

    Menampilkan Data JSON (lv1)

---
+++ Jonathan_SMKN_4_Malang/MenampilkanDataDalamFormatJSON/MenampilkanDataDalamFormatJSON/Controllers/DataDiriSaya.cs
@@ -0,0 +1,25 @@
+﻿using MenampilkanDataDalamFormatJSON.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenampilkanDataDalamFormatJSON.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataDiriSaya : ControllerBase
+    {
+       [HttpGet("/data")]
+       public IActionResult GetDataDiri()
+        {
+            var datadiri = new DataDiri
+            {
+                Nama = "Jonathan Emmanuel Kristanto",
+                Tanggal_Lahir = "1 Januari 2007",
+                Asal_Sekolah = "SMKN 4 Malang",
+                Cita_Cita = "Programer"
+            };
+
+            return Ok(datadiri);
+        }
+    }
+}
---
+++ Jonathan_SMKN_4_Malang/MenampilkanDataDalamFormatJSON/MenampilkanDataDalamFormatJSON/Models/DataDiri.cs
@@ -0,0 +1,11 @@
+﻿namespace MenampilkanDataDalamFormatJSON.Models
+{
+    public class DataDiri
+    {
+        public int Id { get; set; }
+        public string Nama { get; set; }
+        public string Tanggal_Lahir { get; set; }
+        public string Asal_Sekolah { get; set; }
+        public string Cita_Cita { get; set; }
+    }
+}
