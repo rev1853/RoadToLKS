0b973c05c361c23c8017637f947703014d3a9d40
ZhayaGT <jonathanemmanuel0107@gmail.com>
2024-01-03 10:51:20 +0700

    Challenge API Dasar

---
+++ Jonathan_SMKN_4_Malang/API_Dasar_C#1/API_Dasar_C#1/Controllers/HelloAPI_Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Dasar_C_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloAPI_Controller : ControllerBase
+    {
+        [HttpGet("/data")]
+        public IActionResult Get()
+        {
+            return Ok("Hello There, this is the GET side");
+        }
+
+        [HttpPost("/data")]
+        public IActionResult Post()
+        {
+            return Ok("Hello There, this is the Post side");
+        }
+
+        [HttpPut("/data")]
+        public IActionResult Put()
+        {
+            return Ok("Hello There, this is the PUT side");
+        }
+
+        [HttpDelete("/data")]
+        public IActionResult Delete()
+        {
+            return Ok("Hello There, this is the Delete side");
+        }
+    }
+}
